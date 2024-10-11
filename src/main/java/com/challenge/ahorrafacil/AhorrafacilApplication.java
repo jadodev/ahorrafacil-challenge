cbbc67fbfd84fa04599c36b34ef8a5a84f305436
Jonathan Urrego <jhonnathan.dev@gmail.com>
2024-10-11 17:46:42 -0500

    init project

---
+++ src/main/java/com/challenge/ahorrafacil/AhorrafacilApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.ahorrafacil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AhorrafacilApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AhorrafacilApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/challenge/ahorrafacil/AhorrafacilApplicationTests.java
@@ -0,0 +1,13 @@
+package com.challenge.ahorrafacil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhorrafacilApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
